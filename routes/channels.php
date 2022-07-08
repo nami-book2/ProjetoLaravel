6ab17ff5dd0197c05e8dbd6dec1542929152b461
nami-book <nicolebentomendonca@gmail.com>
2022-07-08 14:06:17 -0300

    teste 11

--- routes/channels.php
+++ routes/channels.php
@@ -10,9 +10,9 @@ use Illuminate\Support\Facades\Broadcast;
 | Here you may register all of the event broadcasting channels that your
 | application supports. The given channel authorization callbacks are
 | used to check if an authenticated user can listen to the channel.
-|
+| teste 11
 */
 
 Broadcast::channel('App.Models.User.{id}', function ($user, $id) {
-    return (int) $user->id === (int) $id;
+    return (int) $user->id === (int) $id;   
 });
